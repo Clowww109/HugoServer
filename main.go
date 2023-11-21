15344dc979f9b560f5efcd38d51b9f7ad45afab0
Павел Степанов <clowww110@gmail.com>
2023-11-21 23:37:25 +0300

    Материал скопирован

--- main.go
+++ main.go
@@ -1,99 +1,53 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/go-chi/chi/middleware"
-	"net/http"
-
 	"github.com/go-chi/chi"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
-
-type User struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-}
-
-var people []User
 
 func main() {
 	r := chi.NewRouter()
 
-	people = append(people, User{"Max", 55})
+	// ...
 
-	r.Use(middleware.Logger)
+	http.ListenAndServe(":8080", r)
+}
 
-	r.Route("/hello", func(r chi.Router) {
-		r.Get("/1", handleRoute1)
-	})
-	r.Route("/sayBye", func(r chi.Router) {
-		r.Get("/b", handleRoute2)
-	})
-	r.Route("/users", func(r chi.Router) {
-		r.Get("/list", handleRoute3)
-		r.Post("/add", handleRoute3)
-		r.Delete("/del", handleRoute3)
-	})
+type ReverseProxy struct {
+	host string
+	port string
+}
 
-	err := http.ListenAndServe(":8080", r)
-	if err != nil {
-		fmt.Println(err)
-		return
+func NewReverseProxy(host, port string) *ReverseProxy {
+	return &ReverseProxy{
+		host: host,
+		port: port,
 	}
 }
 
-func handleRoute1(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Say hello!")
+func (rp *ReverseProxy) ReverseProxy(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
 }
 
-func handleRoute2(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Say goodBye!")
-}
+const content = ``
 
-func handleRoute3(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		if r.URL.Path == "/users/list" {
-			data, err := json.Marshal(people)
+func WorkerTest() {
+	t := time.NewTicker(1 * time.Second)
+	var b byte = 0
+	for {
+		select {
+		case <-t.C:
+			err := os.WriteFile("/app/static/_index.md", []byte(fmt.Sprintf(content, b)), 0644)
 			if err != nil {
-				fmt.Println(err)
-				return
+				log.Println(err)
 			}
-			w.Header().Set("Content-Type", "application/json")
-			w.Write(data)
-		} else {
-			fmt.Fprintf(w, "Error, wrong GET")
-		}
-	case "POST":
-		if r.URL.Path == "/users/add" {
-			fmt.Println(r.URL.Path)
-			var user User
-			err := json.NewDecoder(r.Body).Decode(&user)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			people = append(people, user)
-			res, _ := json.Marshal(people)
-			fmt.Fprintf(w, string(res))
-		}
-
-	case "DELETE":
-		if r.URL.Path == "/users/del" {
-			var user User
-			err := json.NewDecoder(r.Body).Decode(&user)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			for i := 0; i < len(people); i++ {
-				if user.Name == people[i].Name {
-					people = append(people[:i], people[i+1:]...)
-					break
-				}
-			}
-			res, _ := json.Marshal(people)
-			fmt.Fprintf(w, string(res))
+			b++
 		}
 	}
 }
